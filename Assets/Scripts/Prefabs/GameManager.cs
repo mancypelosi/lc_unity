2bdc2cec59b6baecda53837bb063e32cea11428c
Bernard Rudi <brudi07@gmail.com>
2018-08-30 21:13:11 -0500

    Updated version to o

--- Assets/Scripts/Prefabs/GameManager.cs
+++ Assets/Scripts/Prefabs/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour {
     public Enemy enemy;
     public World world;
     public string previousScene = "";
-    public static string version = "0.0.1n";
+    public static string version = "0.0.1o";
 
     // Use this for initialization
     void Start()
